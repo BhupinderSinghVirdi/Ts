802f39c706f5982c4c944d9e1b033e63c399b484
BhupinderSinghVirdi <n01435365@humbermail.ca>
2023-01-10 23:55:28 -0500

    first commit

---
+++ week1/main.js
@@ -0,0 +1,7 @@
+var message = 'hello world';
+console.log(message);
+var myUser = {
+    id: 4,
+    name: 'Bhupinder',
+    email: 'hahh@lol.com'
+};
---
+++ week1/main.ts
@@ -0,0 +1,14 @@
+var message: string  = 'hello world';
+console.log(message);
+
+interface User {
+    id: number,
+    name: string,
+    email: string
+}
+
+var myUser: User = {
+    id: 4,
+    name: 'Bhupinder',
+    email: 'hahh@lol.com'
+}
